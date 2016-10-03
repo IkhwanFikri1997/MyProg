b9982edcd7ff61a9b0e0dd1e3d78b558f734b66f
IkhwanFikri1997 <ikhwan.fikri@yahoo.co.id>
2016-10-03 16:47:11 +0800

    Add files via upload

---
+++ DaysOut.cpp
@@ -0,0 +1,21 @@
+int NumberEmployees(int a){
+	cin a;
+		if (a < 1){
+			cout << "invalid data";
+		}
+}
+
+int NumberDays(int a,int b){
+	for(int i=1;i<=a;i++){
+		cout << "How many days that employee" << i << " missed?";
+		cin >> b;
+		if (b<0){
+			cout << "invalid data";
+		}
+	}
+}
+
+int AverageAbsent(int a,int b){
+	int j;
+	j=b/a;
+}
